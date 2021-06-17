b3cb630ff7bd5959edf2659baeba8f4bb83c74ae
Jacob Rohde Jensen <jaco610s@edu.campusvejle.dk>
2021-06-17 11:06:06 +0200

    initial commit

---
+++ V2/JavaScript-opg/js-img-carousel/script.js
@@ -0,0 +1,49 @@
+let slideIndex = 0
+var timer
+changeSlide(1)
+
+let dots = document.querySelectorAll('.dot')
+
+document.querySelector('.prev').addEventListener('click', function(){
+    changeSlide(-1)
+})
+
+document.querySelector('.next').addEventListener('click', function(){
+    changeSlide(1)
+})
+
+for (i of dots){
+    i.addEventListener('click', function(e){
+        showSlides(slideIndex = e.target.id)
+        timer = setTimeout(autoSlideChange, 3000)
+    })
+}
+
+function autoSlideChange(){
+    changeSlide(1)
+}
+
+function changeSlide(n){
+    showSlides(slideIndex += n)
+    timer = setTimeout(autoSlideChange, 3000)
+}
+
+function showSlides(n){
+    clearTimeout(timer)
+    let slides = document.querySelectorAll('.slides')
+    let dots = document.querySelectorAll('.dot')
+
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+
+    for (let i = 0; i < slides.length; i++){
+        slides[i].style.display = "none"
+    }
+
+    for (let i = 0; i < dots.length; i++){
+        dots[i].className = dots[i].className.replace(" active", "")
+    }
+
+    slides[slideIndex-1].style.display = "block"
+    dots[slideIndex-1].className += " active"
+}
